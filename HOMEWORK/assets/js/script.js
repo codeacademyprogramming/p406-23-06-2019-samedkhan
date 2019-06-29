ef67fe29f1116581827306ea4b17b6bbeb3e6514
samedkhan <samadakh@code.edu.az>
2019-06-29 22:18:21 +0400

    formatted all CSS files

--- HOMEWORK/assets/js/script.js
+++ HOMEWORK/assets/js/script.js
@@ -1,8 +1,6 @@
 
 
 var menu_button = document.querySelector(('.menu_list'), ':before');
-var menu_list = document.querySelector(".menu_list ul");
-
 
 
 menu_button.addEventListener("click", function(){ 
